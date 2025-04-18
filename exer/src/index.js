087f8053d68ea91ea9232f8c01d1c605708a7e92
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-18 14:28:11 -0300

    feat: implement first functional component

--- exer/src/index.js
+++ exer/src/index.js
@@ -1,5 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import FirstComp from './components/FirstComp'
 
-const elemento = document.getElementById("root");
-ReactDOM.render("olá rec", elemento);
+const elemento = document.getElementById('root')
+
+ReactDOM.render(
+    <div>
+        <FirstComp/>
+    </div>, elemento)
