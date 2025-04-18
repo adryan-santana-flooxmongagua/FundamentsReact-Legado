b6f818caff6bbffc31e96a2f6a91cff3865c868b
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-18 14:41:17 -0300

    feat: add multiple components in a single file

--- exer/src/index.js
+++ exer/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import FirstComp from './components/FirstComp'
-
+import { CompA, CompB } from './components/Two'
 const elemento = document.getElementById('root')
 
 ReactDOM.render(
     <div>
         <FirstComp/>
+        <CompA valor="ASD"/>
+        <CompB valor="BSD"/>
     </div>, elemento)
